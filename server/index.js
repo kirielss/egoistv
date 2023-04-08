718c24ce2cfaf252c83691c8689c03b5cb6d9a30
skiriel <gabrieldllopes@gmail.com>
2023-04-07 22:19:50 -0300

    feat: join room message

--- server/index.js
+++ server/index.js
@@ -18,6 +18,11 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
 
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    })
+
     socket.on("disconnect", () => {
         console.log("user disconnected", socket.id);
     });
